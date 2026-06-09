951b8c2f8bc2ef71199c62f63bdb00d63f2e71d6
nunomi0 <ykys1@naver.com>
2026-06-09 23:14:55 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/120836

---
+++ 260609/[프로그래머스][level 0] 순서쌍의 개수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i = 1; i*i<=n; i++){
+        if (n%i==0) answer+=2;
+        if (i*i==n) answer-=1;
+    }
+    return answer;
+}

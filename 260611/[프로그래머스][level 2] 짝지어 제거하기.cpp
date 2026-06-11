27453bfa664189cd4d821b99490ba5abadf4018f
nunomi0 <ykys1@naver.com>
2026-06-11 22:24:47 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/12973

---
+++ 260611/[프로그래머스][level 2] 짝지어 제거하기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> st;
+    for (int i = 0; i<s.length(); i++) {
+        if (!st.empty() && st.top()==s[i]) st.pop();
+        else st.push(s[i]);
+    }
+    return st.empty();
+}

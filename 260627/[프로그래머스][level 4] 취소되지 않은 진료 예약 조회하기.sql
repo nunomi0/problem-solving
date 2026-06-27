7203ca8bd8a867d835aacd9f6bf9b6143e4609a8
nunomi0 <ykys1@naver.com>
2026-06-27 21:04:13 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/132204

---
+++ 260627/[프로그래머스][level 4] 취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,20 @@
+SELECT
+    A.APNT_NO,
+    P.PT_NAME,
+    P.PT_NO,
+    A.MCDP_CD,
+    D.DR_NAME,
+    A.APNT_YMD
+FROM
+    PATIENT P
+    JOIN
+    APPOINTMENT A
+        ON P.PT_NO = A.PT_NO
+    JOIN
+    DOCTOR D
+        ON A.MDDR_ID = D.DR_ID
+WHERE
+    TO_CHAR(A.APNT_YMD,'YYYY-MM-DD')='2022-04-13'
+    AND
+    A.APNT_CNCL_YN='N'
+ORDER BY A.APNT_YMD;

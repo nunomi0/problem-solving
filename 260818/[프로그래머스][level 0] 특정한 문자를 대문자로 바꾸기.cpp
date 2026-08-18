54185691af8bfe97cd80b165dd48916956fdfada
nunomi0 <ykys1@naver.com>
2026-08-18 17:03:54 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/181873

---
+++ 260818/[프로그래머스][level 0] 특정한 문자를 대문자로 바꾸기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string alp) {
+    string answer = "";
+    
+    for (int i = 0; i<my_string.length(); i++){
+        if (my_string[i]==alp[0]) answer+=alp[0]+('A'-'a');
+        else answer+=my_string[i];
+    }
+    
+    return answer;
+}

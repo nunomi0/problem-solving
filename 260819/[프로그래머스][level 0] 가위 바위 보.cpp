3faf18590ae549f34b7281686b0b2c3784b8bb9e
nunomi0 <ykys1@naver.com>
2026-08-19 11:12:52 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/120839

---
+++ 260819/[프로그래머스][level 0] 가위 바위 보.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    
+    for (int i = 0; i<rsp.length(); i++){
+        if (rsp[i]=='2') answer+='0';
+        if (rsp[i]=='0') answer+='5';
+        if (rsp[i]=='5') answer+='2';
+    }
+    
+    return answer;
+}

475ba6a0a716f80fd3da369665826677bcd5a35c
nunomi0 <ykys1@naver.com>
2026-09-12 11:55:45 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/131120

---
+++ 260912/[프로그래머스][level 2] 3월에 태어난 여성 회원 목록 출력하기_2.sql
@@ -0,0 +1,4 @@
+select member_id, member_name, gender, to_char(date_of_birth,'YYYY-MM-DD')
+from member_profile
+where to_char(date_of_birth,'MM')='03' and gender='W' and tlno is not null
+order by member_id
